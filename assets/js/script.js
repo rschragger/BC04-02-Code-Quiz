fb73bc44235bd977eb606014f67c1802a6bd6d2f
rschragger <80379457+rschragger@users.noreply.github.com>
2022-06-11 14:22:07 +1000

    220511 14:20 Updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 //Create variables ---------------------------------------------------------
 //var IDs
+//Screens
 var screens = document.getElementById("screens");
 var screensDiv = document.querySelectorAll("#screens div");
 var homeScreen = document.getElementById("home-screen");
@@ -7,6 +8,10 @@ var questionScreen = document.getElementById("question-screen");
 var resultScreen = document.getElementById("result-screen");
 var highScoreScreen = document.getElementById("high-score-screen");
 var settingsScreen = document.getElementById("settings-screen");
+
+//spans
+var timeForQuizSpan = document.getElementById("time-for-quiz");
+var timeDeductedSpan = document.getElementById("time-deducted");
 
 // var Buttons
 var highScoresBtn = document.querySelector("high-score-btn");
@@ -16,35 +21,46 @@ var startBtn = document.querySelector("start-btn");
 
 // Functions ---------------------------------------------------------
 function init() {
- clearScreens()
-  goToHomeScreen()
+  //  clearScreens() ;
+  goToHomeScreen();
 
   //Use section below to test functions whilst developing
-  
-  
+  goToSettingsScreen();
+
+
 }
 
 //Clear Screens - this function makes all screen divs disappear so that we can turn on the screen we want
-function clearScreens(){  
-for(i=0; i<screensDiv.length ; i++){
-  console.log(screensDiv[i]);
-  screensDiv[i].setAttribute("style", "display:none");
-}}
+function clearScreens() {
+  for (i = 0; i < screensDiv.length; i++) {
+    //We need to check that only "screen" IDs are hidden
+    var checkScreen = String(screensDiv[i].id);
+    // console.log(checkScreen)
+    if (checkScreen.slice(- 6) == "screen") {
+      screensDiv[i].setAttribute("style", "display:none")
+    };
+  }
+} //This is (should be) referenced in all goTo functions
 
 //Show other screens functions - - - -
-function goToHomeScreen(){
+function goToHomeScreen() {
+  clearScreens()
   homeScreen.setAttribute("style", "display:block");
 }
-function goToQuestionScreen(){
+function goToQuestionScreen() {
+  clearScreens()
   questionScreen.setAttribute("style", "display:block");
 }
-function goToResultScreen(){
+function goToResultScreen() {
+  clearScreens()
   resultScreen.setAttribute("style", "display:block");
 }
-function goToHighScoreScreen(){
+function goToHighScoreScreen() {
+  clearScreens()
   highScoreScreen.setAttribute("style", "display:block");
 }
-function goToHighsettingsScreen(){
+function goToSettingsScreen() {
+  clearScreens()
   settingsScreen.setAttribute("style", "display:block");
 }
 //End show other screens functions - - - -
